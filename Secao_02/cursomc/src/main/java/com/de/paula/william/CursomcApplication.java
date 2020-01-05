315aae87e110ed746b8eef96b665b89aaefff1bc
William de Paula <williampereiradepaula@gmail.com>
2020-01-05 18:29:27 -0400

    Projeto criado

---
+++ Secao_02/cursomc/src/main/java/com/de/paula/william/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.de.paula.william;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
