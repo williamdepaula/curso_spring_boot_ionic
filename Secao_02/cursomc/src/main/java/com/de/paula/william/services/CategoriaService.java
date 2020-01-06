543dcdc1a7d791b137ca4326305c389c8159eb50
William de Paula <williampereiradepaula@gmail.com>
2020-01-06 16:46:55 -0400

    Ajustes no endpoint /categoria/{id}

--- Secao_02/cursomc/src/main/java/com/de/paula/william/services/CategoriaService.java
+++ Secao_02/cursomc/src/main/java/com/de/paula/william/services/CategoriaService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.de.paula.william.domain.Categoria;
 import com.de.paula.william.repositories.CategoriaRepository;
+import com.de.paula.william.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -16,6 +18,8 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()
+				));
 	}
 }
