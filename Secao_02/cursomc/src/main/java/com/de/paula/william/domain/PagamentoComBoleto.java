df120f7c1e334e8be5509df8fc43878e2df2db76
William de Paula <williampereiradepaula@gmail.com>
2020-01-07 18:01:21 -0400

    Endpoint /pedidos/{id} diponibilizado

--- Secao_02/cursomc/src/main/java/com/de/paula/william/domain/PagamentoComBoleto.java
+++ Secao_02/cursomc/src/main/java/com/de/paula/william/domain/PagamentoComBoleto.java
@@ -5,12 +5,16 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.de.paula.william.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern = "dd/MM/yyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern = "dd/MM/yyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
