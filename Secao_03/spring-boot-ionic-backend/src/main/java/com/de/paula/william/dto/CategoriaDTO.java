38d449031fc61bd4e248e03eb29f5e87166f1aae
William de Paula <williampereiradepaula@gmail.com>
2020-01-08 16:14:23 -0400

    Validação sintatica com Bean validation

--- Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/dto/CategoriaDTO.java
+++ Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.de.paula.william.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.de.paula.william.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ter entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/services/CategoriaService.java
+++ Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.de.paula.william.domain.Categoria;
+import com.de.paula.william.dto.CategoriaDTO;
 import com.de.paula.william.repositories.CategoriaRepository;
 import com.de.paula.william.services.exceptions.DataIntegrityException;
 import com.de.paula.william.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 	
