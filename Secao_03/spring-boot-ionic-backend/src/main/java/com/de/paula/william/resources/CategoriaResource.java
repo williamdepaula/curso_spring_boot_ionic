3782b6b21ceb9a6a8fc9c88cde1fdf069b34b3e1
William de Paula <williampereiradepaula@gmail.com>
2020-01-08 14:59:08 -0400

    Listando todas as categorias com DTO

--- Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/resources/CategoriaResource.java
+++ Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.de.paula.william.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.de.paula.william.domain.Categoria;
+import com.de.paula.william.dto.CategoriaDTO;
 import com.de.paula.william.services.CategoriaService;
 
 @RestController
@@ -50,5 +53,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
