11f57393c5e29b58008f0a0e3a1cf2e6e68e3e8f
William de Paula <williampereiradepaula@gmail.com>
2020-01-09 14:21:38 -0400

    Ajustando e testando a deleção de cliente

--- Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/services/ClienteService.java
+++ Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir por que há entidades relacionadas");
+			throw new DataIntegrityException("Não é possivel excluir por que há pedidos relacionadas");
 		}
 		
 	}
