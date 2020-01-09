1efb2e9589c62972508cb1b2f7298804ba137400
William de Paula <williampereiradepaula@gmail.com>
2020-01-09 14:16:19 -0400

    Inserindo um novo cliente com POST

--- Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/resources/ClienteResource.java
+++ Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.de.paula.william.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.de.paula.william.domain.Cliente;
 import com.de.paula.william.dto.ClienteDTO;
+import com.de.paula.william.dto.ClienteNewDTO;
 import com.de.paula.william.services.ClienteService;
 
 @RestController
@@ -31,6 +34,17 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/services/ClienteService.java
+++ Secao_03/spring-boot-ionic-backend/src/main/java/com/de/paula/william/services/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.de.paula.william.domain.Cidade;
 import com.de.paula.william.domain.Cliente;
+import com.de.paula.william.domain.Endereco;
+import com.de.paula.william.domain.enums.TipoCliente;
 import com.de.paula.william.dto.ClienteDTO;
+import com.de.paula.william.dto.ClienteNewDTO;
 import com.de.paula.william.repositories.ClienteRepository;
+import com.de.paula.william.repositories.EnderecoRepository;
 import com.de.paula.william.services.exceptions.DataIntegrityException;
 import com.de.paula.william.services.exceptions.ObjectNotFoundException;
 
@@ -22,12 +27,22 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
 				));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -57,6 +72,22 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogadouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if (objDTO.getTelefone2()!=null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if (objDTO.getTelefone3()!=null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
